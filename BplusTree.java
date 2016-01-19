71872b7dcaf86879dc1534751087b53470653ac7
JagatBhat09 <jagatbhat09@gmail.com>
2016-01-19 15:48:28 +0530

    this is the third commit with complete BLink tree

--- BplusTree.java
+++ BplusTree.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 //a global variable class used for assigning uniq node number for each node
 class GlobalVariables{
-    public static int node_no;
+    public static int node_no,no_keys;
 
 }
 
@@ -19,6 +19,7 @@ class BPnode{
     public BPnode(int n_key){
         int i;
         this.nt = 'e';
+        System.out.println(GlobalVariables.node_no);
         this.no = GlobalVariables.node_no++;
         link = new BPnode[n_key+1];
         key = new int[n_key];
@@ -146,16 +147,21 @@ class BPT{
 
     public BPnode split_node(BPnode parent,BPnode father, BPnode child, int item,int no_items_father){
         int temp_keys[] = combine_array(child,item),middle_key = (n_key+1)/2,i,j,ind,val=1;
+        // locking the child 
+
+
+
         if(child.is_leaf_node())
             val = 0;
         if(child.is_leaf_node() && no_items_father < n_key){
-            BPnode s_node = child.yet_to_be_named(temp_keys,n_key,val);
-            if(no_items_father < n_key) {
-                father.insert_item_into_node(temp_keys[middle_key],father.get_no_items(n_key)); 
-                father.shift_links(((ind = father.find_index_of_item(temp_keys[middle_key]))+1));
-                father.link[ind+1] = s_node;
-                return parent;
-            }    
+                 BPnode s_node = child.yet_to_be_named(temp_keys,n_key,val);
+                if(no_items_father < n_key) {
+                    father.insert_item_into_node(temp_keys[middle_key],father.get_no_items(n_key)); 
+                    father.shift_links(((ind = father.find_index_of_item(temp_keys[middle_key]))+1));
+                    father.link[ind+1] = s_node;
+                    return parent;
+                }    
+            
         }
         else {
 
@@ -174,15 +180,16 @@ class BPT{
                 if( !child.is_leaf_node() ){//&&  child.link[0].is_leaf_node()){ 
                     System.out.println(child.link[0].is_leaf_node());
                     father_keys = combine_array(father,item);
-                    s_node = child;   //got a problem here      
+                    s_node = child;         
 
                 }
                 else
                     s_node = child.yet_to_be_named(temp_keys,n_key,val);   //got a problem here      
                 BPnode fs_node = father.yet_to_be_named(father_keys,n_key,1);
+                fs_node.elink = father.elink;
+                father.elink = fs_node;
                 int pos_middle_key = father.find_index_of_item(father_keys,temp_keys[middle_key]);
                 if(pos_middle_key == middle_key){
-
                     System.out.println("step 1");//+pos_middle_key);
                     j = middle_key+1;
                     fs_node.link[0] = s_node;
@@ -193,7 +200,7 @@ class BPT{
                 }
                 else
                     if(pos_middle_key < middle_key ){
-                    System.out.println("step 2");
+                        System.out.println("step 2");
                         j = middle_key;
                         for(i=0;j<=n_key;i++,j++){
                             fs_node.link[i] = father.link[j];
@@ -201,8 +208,6 @@ class BPT{
                         }
                         father.shift_links(((ind = father.find_index_of_item(temp_keys[middle_key]))+1));
                         father.link[ind+1] = s_node;
-                    //    System.out.println(ind+"ind"+father.link[2].key[0]+"father key"+fs_node.key[0]);
-                        //yet to be completed
                     }
                     else{
                         System.out.println("step 3");
@@ -215,155 +220,143 @@ class BPT{
                         fs_node.link[ind+1] = s_node;
                     }
                 BPnode parent_of_father = find_parent(parent,father);
-
                 if(father == parent && no_items_father == n_key)
                 {
-                    System.out.println("hit");
-
                     BPnode god_father = new BPnode(n_key);
+                    
                     god_father.key[0] = father_keys[middle_key]; //??
                     god_father.link[0] = father;
                     god_father.link[1] = fs_node ;
+                    if(parent.is_leaf_node()){
+                    System.out.println("god father and its childres no "+god_father.no+father.no+s_node.no);
+                        father.elink = s_node;
+                        god_father.link[1] = s_node;
+                    }
                     return god_father;
                 }   
-
                 return split_node(parent,parent_of_father,fs_node,father_keys[middle_key],parent_of_father.get_no_items(n_key));
+                }
+            }
+            System.out.println("It shud not come here");
+            return parent;
+
+        }
+
+        //recursive method which traverse and inserts the item in correct place! splitting is done if necessary
+        public BPnode insert(BPnode parent,BPnode root,int item){
+
+            int n_items_in_rnode = root.get_no_items(n_key),i;
+            BPnode father = find_parent(parent,root);
+            int n_item_in_fnode = father.get_no_items(n_key);
+
+            //if node is external/leaf node
+            if(root.is_leaf_node()){//root.link[0] == null){
+
+                //*****  Call move right if necessary and make sure that it should be unblocked once insertion is successful
+                //root.move_right();
+                //if external node is not full
+                if(n_items_in_rnode < this.n_key){
+                   root.insert_item_into_node(item,n_items_in_rnode);
+                   return parent;
+                }
+
+                //if external node is full 
+                if(n_items_in_rnode == this.n_key ){
+                    return  split_node(parent,father,root,item,n_item_in_fnode);
+                }
+            }
+            else {
+                i=0;
+                while(i!=n_key && root.key[i] != -999)
+                    if(root.key[i] > item)
+                        return insert(parent,root.link[i],item);
+                    else
+                        i+=1;
+                return insert(parent,root.link[i],item);
+            }
+            System.out.println("IT should not come here");
+            return parent;
+            }
+
+            public BPnode insert(BPnode parent,int item)
+            {
+                return insert(parent,parent,item);
+            }
+
+
+            //preorder traversal
+            public void traversal(BPnode root){
+                int i;
+                for(i=0;i<n_key && (root.key[i] != -999); i++)
+                    System.out.println(root.key[i]+" "+root.no);
+                for(i=0;i<n_key+1 && (root.link[i] != null ); i++)
+                    traversal(root.link[i]);
+            }
+            //preorder traversal and dumping it into the file "BTREE.dot"
+            public void preorderDump(BPnode root,PrintWriter out) {
+                out.print(root.no+" [label=\"");
+                for(int i=0;i<n_key && root.key[i] != -999 ;i++)
+                    out.print(root.key[i]+" ");
+                out.print("\"]\n");
+     //           for(BPnode temp = root; temp.elink!=null ;temp= temp.elink)
+   //                  out.println(temp.no+"->"+temp.elink.no+";");
+                if(root.link[0] != null){
+                    for(int i=0;(i<n_key+1 && root.link[i] != null);i++)
+                        out.println(root.no+"->"+root.link[i].no+";");
+                    for(int i=0;i<=n_key && root.link[i] != null;i++)
+                        preorderDump(root.link[i],out);
+                }
+                
 
             }
-        }
-        System.out.println("It shud not come here");
-        return parent;
-
-    }
-
-    //recursive method which traverse and inserts the item in correct place! splitting is done if necessary
-    public BPnode insert(BPnode parent,BPnode root,int item){
-
-        int n_items_in_rnode = root.get_no_items(n_key),i;
-        BPnode father = find_parent(parent,root);
-        int n_item_in_fnode = father.get_no_items(n_key);
-
-        //if node is external/leaf node
-        if(root.link[0] == null){
-
-
-            //if external node is not full
-            if(n_items_in_rnode < this.n_key){
-                root.insert_item_into_node(item,n_items_in_rnode);
-                return parent;
+            public void exterlinkDump(BPnode root,PrintWriter out) {
+                for(BPnode temp = root.link[0];temp!=null;temp = temp.link[0]){
+                    for(BPnode t = temp; t.elink!=null;t = t.elink)
+                        out.println(t.no+"->"+t.elink.no+";");
+                    out.print(" {rank = same; ");
+                    for(BPnode t = temp; t!=null;t = t.elink)
+                        out.print(t.no+";");
+                    out.print("}");
+                }
             }
 
-            //if external node is full but it's parent is not @$$$%% will decide to do it generic later
-            if(n_items_in_rnode == this.n_key ){
-                return  split_node(parent,father,root,item,n_item_in_fnode);
+            //method which creates a new file, and make it ready for dumping the tree
+            public void display(BPnode root) {
+                try {
+                    PrintWriter out = new PrintWriter(new FileWriter("BTREE.dot",true));
+                    out.println("digraph btree {\n");
+                    preorderDump(root,out);
+                    exterlinkDump(root,out);
+                    out.println("}\n");
+                    out.close();
+                    Runtime rt = Runtime.getRuntime();
+                    rt.exec("dot -Tps -O BTREE.dot");
+                    rt.exec("gnome-open BTREE.dot.ps &");
+                }
+                catch( Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+        }
+
+        public class BplusTree{
+            public static void main(String[] args){
+                Scanner in = new Scanner(System.in); 
+                GlobalVariables.no_keys = in.nextInt();
+                BPT op = new BPT(GlobalVariables.no_keys);
+                BPnode root = new BPnode(GlobalVariables.no_keys);
+                BPnode parent;
+                parent = root;
+                int i=0,k;
+                while(i<10000){
+                    k = in.nextInt();
+                    System.out.println("INput Value is"+ k);
+                    parent = op.insert(parent,k);
+                    i++;
+                }
+                    op.display(parent);
+                System.out.println(parent.no);
             }
         }
-        else {
-            i=0;
-            while(i!=n_key && root.key[i] != -999)
-                if(root.key[i] > item)
-                    return insert(parent,root.link[i],item);
-                else
-                    i+=1;
-            return insert(parent,root.link[i],item);
-        }
-        System.out.println("IT should not come here");
-        return parent;
-    }
-
-    public BPnode insert(BPnode parent,int item)
-    {
-        return insert(parent,parent,item);
-    }
-
-
-    //preorder traversal
-    public void traversal(BPnode root){
-        int i;
-        for(i=0;i<n_key && (root.key[i] != -999); i++)
-            System.out.println(root.key[i]+" "+root.no);
-        for(i=0;i<n_key+1 && (root.link[i] != null ); i++)
-            traversal(root.link[i]);
-    }
-    //preorder traversal and dumping it into the file "BTREE.dot"
-    public void preorderDump(BPnode root,PrintWriter out) {
-        //System.out.println("hi")
-        out.print(root.no+" [label=\"");
-        for(int i=0;i<n_key && root.key[i] != -999 ;i++)
-            out.print(root.key[i]+" ");
-        out.print("\"]\n");
-        if(root.link[0] != null){
-            for(int i=0;(i<n_key+1 && root.link[i] != null);i++)
-                out.println(root.no+"->"+root.link[i].no+";");
-            for(int i=0;i<=n_key && root.link[i] != null;i++)
-                preorderDump(root.link[i],out);
-        }
-
-    }
-
-    //method which creates a new file, and make it ready for dumping the tree
-    public void display(BPnode root) {
-        try {
-            PrintWriter out = new PrintWriter(new FileWriter("BTREE.dot",true));
-            out.println("digraph btree {\n");
-            preorderDump(root,out);
-            out.println("}\n");
-            out.close();
-            Runtime rt = Runtime.getRuntime();
-            rt.exec("dot -Tps -O BTREE.dot");
-            rt.exec("gnome-open BTREE.dot.ps &");
-        }
-        catch( Exception ex) {
-            ex.printStackTrace();
-        }
-    }
-
-}
-
-public class BplusTree{
-    public static void main(String[] args){
-        BPT op = new BPT(3);
-        BPnode root = new BPnode(3);
-        BPnode parent;
-        parent = root;
-        BPnode n1 = new BPnode(3);
-        BPnode n2 = new BPnode(3);
-        BPnode n3 = new BPnode(3);
-        BPnode n4 = new BPnode(3);
-        BPnode n5 = new BPnode(3);
-        BPnode n6 = new BPnode(3);
-        root.nt = 'i';
-        n1.nt = 'i';
-        n2.nt = 'i';
-        root.key[0] = 50;
-        root.link[0] = n1;
-        root.link[1] = n2;
-        n1.key[0] = 25;
-        n2.key[0] = 75;
-        n1.link[0] = n3;
-        n1.link[1] = n4;
-        n2.link[0] = n5;
-        n2.link[1] = n6;
-
-        n3.key[0] = 12;
-        n4.key[0] = 25;
-        n4.key[1] = 37;
-        n5.key[0] = 50;
-        n5.key[1] = 60;
-        n6.key[0] = 75;
-        n6.key[1] = 100;
-        n3.elink = n4;
-        n4.elink = n5;
-        n5.elink = n6;
-        Scanner in = new Scanner(System.in); 
-        int i=0,k;
-        while(i<1000){
-            k = in.nextInt();
-            System.out.println("INput Value is"+ k);
-            parent = op.insert(parent,k);
-            i++;
-        }
-            op.display(parent);
-    }
-}
 
